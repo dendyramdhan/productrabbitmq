74e53db09744d40fa6738788a5718b0890a6355d
Dendy Ramdhan <ramdhandendy@gmail.com>
2024-06-24 15:49:10 +0700

    Delete Swagger config, Update rabbitmq config and added test controller on sender

--- productrabbitmq-receiver/src/main/java/com/dendyramdhan/productrabbitmq/receiver/config/RabbitMQConfig.java
+++ productrabbitmq-receiver/src/main/java/com/dendyramdhan/productrabbitmq/receiver/config/RabbitMQConfig.java
@@ -1,8 +1,13 @@
 package com.dendyramdhan.productrabbitmq.receiver.config;
 
+import com.dendyramdhan.productrabbitmq.receiver.service.ProductListenerService;
 import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 
 @Configuration
 public class RabbitMQConfig {
@@ -12,6 +17,29 @@ public class RabbitMQConfig {
     @Bean
     public Queue queue() {
         return new Queue(QUEUE, false);
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory,
+                                                                   MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames("product_queue");
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    public MessageListenerAdapter listenerAdapter(ProductListenerService receiver,
+                                                  Jackson2JsonMessageConverter converter) {
+        MessageListenerAdapter adapter = new MessageListenerAdapter(receiver, "receive");
+        adapter.setMessageConverter(converter);
+        return adapter;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
 
--- productrabbitmq-sender/src/main/java/com/dendyramdhan/productrabbitmq/sender/config/RabbitMQConfig.java
+++ productrabbitmq-sender/src/main/java/com/dendyramdhan/productrabbitmq/sender/config/RabbitMQConfig.java
@@ -3,6 +3,9 @@ package com.dendyramdhan.productrabbitmq.sender.config;
 import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
 @Configuration
 public class RabbitMQConfig {
@@ -12,6 +15,18 @@ public class RabbitMQConfig {
     @Bean
     public Queue queue() {
         return new Queue(QUEUE, false);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
 
