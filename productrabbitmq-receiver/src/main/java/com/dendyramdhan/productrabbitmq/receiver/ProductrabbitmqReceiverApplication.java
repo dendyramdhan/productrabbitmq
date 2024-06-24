c5b804868422b3d95712cd93cb53d06c487e4ad5
Dendy Ramdhan <ramdhandendy@gmail.com>
2024-06-24 14:43:29 +0700

    Update Dockerfile and add swagger on application both sender and receiver

--- productrabbitmq-receiver/src/main/java/com/dendyramdhan/productrabbitmq/receiver/ProductrabbitmqReceiverApplication.java
+++ productrabbitmq-receiver/src/main/java/com/dendyramdhan/productrabbitmq/receiver/ProductrabbitmqReceiverApplication.java
@@ -2,8 +2,11 @@ package com.dendyramdhan.productrabbitmq.receiver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Product API Receiver", version = "1.0", description = "Documentation APIs v1.0"))
 public class ProductrabbitmqReceiverApplication {
 
 	public static void main(String[] args) {
--- productrabbitmq-sender/src/main/java/com/dendyramdhan/productrabbitmq/sender/ProductrabbitmqSenderApplication.java
+++ productrabbitmq-sender/src/main/java/com/dendyramdhan/productrabbitmq/sender/ProductrabbitmqSenderApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(info = @Info(title = "Product API Sender", version = "1.0", description = "Documentation APIs v1.0"))
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class ProductrabbitmqSenderApplication {
 
